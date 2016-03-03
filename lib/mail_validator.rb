fb4ddd6568e0984ae6246ae4ddbed09ab04a573c
Felipe Munhoz <felipe@felipemunhoz.com>
2016-03-03 08:55:32 -0300

    Added default error message
    Same solution from here: https://github.com/sobrinho/cnpj_validator/commit/0e7a4d39d83cb7166804f7aa3d800e40eb268856

--- lib/mail_validator.rb
+++ lib/mail_validator.rb
@@ -1,7 +1,8 @@
 class MailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value !~ /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-      record.errors.add(attribute, options[:message])
+      message = options[:message].presence || :invalid
+      record.errors.add(attribute, message)
     end
   end
 end
